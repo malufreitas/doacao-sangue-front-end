ff82e70dae4421fe572c37398104f26b5662e28c
Guilherme Maciel <guilhermemaciel2@hotmail.com>
2019-05-14 18:26:49 -0300

    Update relatorio1.ts

--- src/app/relatorio/relatorio1/relatorio1.ts
+++ src/app/relatorio/relatorio1/relatorio1.ts
@@ -1,12 +1,5 @@
 export interface Relatorio1 {
-    id: number;
-    nome: string;
-    quantidade: number;
-    descricao: string;
-    valorunitario: Float32Array;
-    categoria: string;
-    tamanho: string;
-    volume: string;
-    material: string;
-    genero: string
+    tipofator: string;
+    hemocentro: string;
+    qtddemanda: number;
   }
