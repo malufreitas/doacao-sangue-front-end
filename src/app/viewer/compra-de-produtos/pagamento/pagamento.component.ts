0711f60d8a38aacd325348aa9af823d850717bb6
malufreitas <malufrt@Hotmail.com>
2019-06-11 21:53:50 -0300

    pagamento boleto

--- src/app/viewer/compra-de-produtos/pagamento/pagamento.component.ts
+++ src/app/viewer/compra-de-produtos/pagamento/pagamento.component.ts
@@ -12,11 +12,23 @@ declare let paypal: any;
 
 export class PagamentoComponent implements AfterViewChecked {
 
+  // Paypal
   addScript: boolean = false;
   paypalLoad: boolean = true;
 
   totalAmount: number = 1;
 
+  endereco_cobranca: {};
+  compra: {};
+  // Paypal
+
+
+  // Boleto fácil
+  
+  // Boleto fácil
+
+
+  // Paypal
   paypalConfig = {
     env: 'sandbox',
 
@@ -28,36 +40,46 @@ export class PagamentoComponent implements AfterViewChecked {
     commit: true,
     payment: (data, actions) => {
 
-      let a = actions.payment.create({
+      return actions.payment.create({
         payment: {
           transactions: [
             // ****** O valor fixo do pagamento vai vir aqui
             { amount: { total: this.totalAmount, currency: 'BRL' } }
           ]
         }
-      });
-      //console.log(a);
-      return a;
-    },
+      });   
 
+    },
 
     onAuthorize: (data, actions) => {
       return actions.payment.execute().then((payment) => {
         //Do something when payment is successful.
         window.alert('Pagamento efetuado!');
-        /*
-        endereco_entrega = {
+
+        this.endereco_cobranca = {
           'rua': payment.payer.payer_info.shipping_address.line1 ,
           'bairro': payment.payer.payer_info.shipping_address.line2,
           'cidade': payment.payer.payer_info.shipping_address.city,
           'estado': payment.payer.payer_info.shipping_address.state,
           'cep': payment.payer.payer_info.shipping_address.postal_code
         };
-        */
+
+        this.compra = {
+          'status': payment.state,  // payment.payer.status ??
+          'data': payment.create_time,
+          'pagamento': payment.payer.payment_method, 
+          'valorTotal': payment.trasactions.amount.total,  //this.totalAmount
+          'idendereco': 0,
+          'idpessoa': 0
+        }
+
         console.log(payment.payer.payer_info.shipping_address);
-        // Ver parte do backend
+        // Passar para o back-end por metodo HTTP
+        // endereco_cobranca
+        // compra
       })
     }
+    
   };
 
 
@@ -80,83 +102,16 @@ export class PagamentoComponent implements AfterViewChecked {
       document.body.appendChild(scripttagElement);
     })
   }
-
-}
-
+  // Paypal
 
 
 
-
-
-
-/*
-// export class PagamentoComponent implements OnInit, AfterViewChecked {
-export class PagamentoComponent implements AfterViewChecked {
-
-  addScript: boolean = false;
-
-  paypalCarregando: boolean = true;    //paypalLoad: boolean = true;
-
-  montanteFinal: number = 1;
-
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-
-  paypalConfiguracao = {
-    env: 'sandbox',
-
-    // Códigos da conta sandbox do paypal
-    client: {
-      // Key
-      sandbox: '<your-sandbox-key-here>',
-      // Secret key
-      production: '<your-production-key here>'
-    },
-
-    commit: true,
-
-    pagamento: (data, actions) => {
-      return actions.pagamento.create({
-        pagamento: {
-          transactions: [
-            { montante: { total: this.montanteFinal, moeda: 'USD' } }
-            // é possível alterar o tipo da moeda
-            //{ amount: { total: this.montanteFinal, moeda: 'BRL' } }
-          ]
-        }
-      });
-    },
-
-    onAuthorize: (data, actions) => {
-      return actions.pagamento.execute().then((pagamento) => {
-        // Faça alguma coisa quando o pagamento é feito com sucesso.
-      })
-    }
-  };
-
-
-
-  ngAfterViewChecked(): void {
-    if (!this.addScript) {
-      this.addPaypalScript().then(() => {
-        paypal.Button.render(this.paypalConfiguracao, '#botao-paypal-checkout');
-        this.paypalCarregando = false;
-      })
-    }
-  }
-
-
-
-  addPaypalScript() {
+  //Boleto Fácil
+  addBoletoScript() {
     this.addScript = true;
     return new Promise((resolve, reject) => {
       let scripttagElement = document.createElement('script');
-      //  <script src="https://www.paypalobjects.com/api/checkout.js"> </script>
-      scripttagElement.src = 'https://www.paypalobjects.com/api/checkout.js';
+      scripttagElement.src = 'https://sandbox.boletobancario.com/boletofacil/wro/direct-checkout.min.js';
       scripttagElement.onload = resolve;
       document.body.appendChild(scripttagElement);
     })
@@ -164,5 +119,5 @@ export class PagamentoComponent implements AfterViewChecked {
 
 
 
+
 }
-*/
