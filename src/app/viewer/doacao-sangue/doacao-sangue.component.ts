961f4a9b8484136c5bf418d299b247694b70b1e2
malufreitas <malufrt@Hotmail.com>
2019-06-25 10:37:01 -0300

    antes do carousel

--- src/app/viewer/doacao-sangue/doacao-sangue.component.ts
+++ src/app/viewer/doacao-sangue/doacao-sangue.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoacaoSangueComponent implements OnInit {
 
-  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+  //images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
 
   onSubmit(form) {
     console.log(form);
